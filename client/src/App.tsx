2108f5a69ffb432f3cc18b3270988bf1ff695886
Nethsara <nethsarasiyum2@gmail.com>
2023-03-06 22:58:45 +0530

    showing on decks on landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
-import React, { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+
+type TDeck = {
+  title: string;
+  _id: string;
+};
 
 function App() {
   const [title, setTitle] = useState("");
+  const [decks, setDecks] = useState<TDeck[]>([]);
 
   async function handleCreateDeck(e: React.FormEvent) {
     console.log(title);
@@ -21,20 +26,41 @@ function App() {
     setTitle("");
   }
 
+  useEffect(() => {
+    async function getData() {
+      await fetch("http://localhost:5000/decks")
+        .then((response) => response.json())
+        .then((res) => {
+          setDecks(res);
+        })
+        .then(() => {
+          console.log("success");
+        });
+    }
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <form onSubmit={handleCreateDeck}>
-        <label htmlFor="deckTitle">Deck Title</label>
-        <input
-          type="text"
-          id="deckTitle"
-          value={title}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setTitle(e.target.value);
-          }}
-        />
-        <button>Create Deck</button>
-      </form>
+    <div className="container">
+      <div className="App">
+        <h1>Your Decks</h1>
+        <ul className="decks">
+          {decks.map((deck) => (
+            <li key={deck._id}>{deck.title}</li>
+          ))}
+        </ul>
+        <form onSubmit={handleCreateDeck}>
+          <label htmlFor="deck-title">Deck Title</label>
+          <input
+            id="deck-title"
+            value={title}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setTitle(e.target.value);
+            }}
+          />
+          <button>Create Deck</button>
+        </form>
+      </div>
     </div>
   );
 }
