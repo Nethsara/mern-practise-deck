c1262853b6a69298eec121f495fac3ccc75de0c0
Nethsara <nethsarasiyum2@gmail.com>
2023-03-06 17:23:13 +0530

    react form submit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState("");
+
+  function handleCreateDeck(e: React.FormEvent) {
+    e.preventDefault();
+    fetch("http://localhost:5000/decks", {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+      }),
+    });
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleCreateDeck}>
+        <label htmlFor="deckTitle">Deck Title</label>
+        <input
+          type="text"
+          id="deckTitle"
+          value={title}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <button>Create Deck</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
